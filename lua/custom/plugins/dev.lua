057a8707585d6a10036c74409f2d244c6d949250
Aamos Pernu <aamos.pernu@outlook.com>
2025-01-16 18:18:57 +0200

    Rewrote config

---
+++ lua/custom/plugins/dev.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        dir = "~/src/plugins/present.nvim",
+        config = function()
+            require("present").setup()
+        end
+    },
+    {
+        dir = "~/src/plugins/spreadsheet.nvim"
+    }
+}
---
+++ lua/custom/plugins/lsp.lua
@@ -0,0 +1,71 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "saghen/blink.cmp",
+            {
+                "folke/lazydev.nvim",
+                opts = {
+                    library = {
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+        },
+        config = function()
+            require("custom.lsp")
+            -- local capabilities = vim.tbl_deep_extend(
+            --     "force",
+            --     {},
+            --     vim.lsp.protocol.make_client_capabilities(),
+            --     require("blink.cmp").get_lsp_capabilities())
+            --
+            -- require("lspconfig").lua_ls.setup({ capabilities = capabilities })
+            -- require("lspconfig").ols.setup({
+            --     capabilities = capabilities,
+            --     init_options = {
+            --         enable_format = true,
+            --         enable_hover = true,
+            --         enable_snippets = true,
+            --         enable_fake_methods = true,
+            --         enable_inlay_hints = true,
+            --         enable_references = true,
+            --         enable_rename = true,
+            --     }
+            -- })
+            --
+            -- require("mason").setup()
+            -- require("mason-lspconfig").setup({
+            --     ensure_installed = {
+            --         "lua_ls",
+            --         "ols",
+            --     }
+            -- })
+            -- require("mason-lspconfig").setup_handlers {
+            --     function(server_name)
+            --         require("lspconfig")[server_name].setup {}
+            --     end
+            -- }
+            --
+            -- vim.api.nvim_create_autocmd("LspAttach", {
+            --     callback = function(args)
+            --         local bufnr = args.buf
+            --         local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+            --         local builtin = require("telescope.builtin")
+            --         vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+            --         vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
+            --         vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
+            --         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+            --         vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+            --         vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+            --
+            --         vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
+            --         vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
+            --         vim.keymap.set("n", "<space>wd", builtin.lsp_document_symbols, { buffer = 0 })
+            --     end
+            -- })
+        end
+    },
+}
